88ff352f9c52b16788b0503be367a4e94a290c8b
PwRafli <rafliefendymm@gmail.com>
2025-09-30 10:02:50 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3/Operator Logika/Operator Logika.js
@@ -0,0 +1,7 @@
+function cekLogika(a, b) {
+    console.log("a && b : " + (a && b)); // AND
+    console.log("a || b : " + (a || b)); // OR
+    console.log("!a : " + (!a));         // NOT
+}
+
+cekLogika(true, false);
