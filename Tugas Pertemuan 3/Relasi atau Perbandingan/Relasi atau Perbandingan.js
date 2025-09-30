0273663579ad5cc362bb2a08d5994020c25e74ac
PwRafli <rafliefendymm@gmail.com>
2025-09-30 10:05:45 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3/Relasi atau Perbandingan/Relasi atau Perbandingan.js
@@ -0,0 +1,12 @@
+function cekRelasi(a, b) {
+    console.log(a + " == " + b + " : " + (a == b));
+    console.log(a + " === " + b + " : " + (a === b));
+    console.log(a + " != " + b + " : " + (a != b));
+    console.log(a + " !== " + b + " : " + (a !== b));
+    console.log(a + " > " + b + " : " + (a > b));
+    console.log(a + " < " + b + " : " + (a < b));
+    console.log(a + " >= " + b + " : " + (a >= b));
+    console.log(a + " <= " + b + " : " + (a <= b));
+}
+
+cekRelasi(5, "5");
