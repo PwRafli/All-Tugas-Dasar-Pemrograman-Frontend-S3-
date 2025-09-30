6e3043547a2a82f12819672d8ec01a9fb57f1640
PwRafli <rafliefendymm@gmail.com>
2025-09-30 10:03:56 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3/Penugasan (Assignment)/Penugasan (Assignment).js
@@ -0,0 +1,21 @@
+function contohAssignment() {
+    let x = 5;
+    console.log("Nilai awal x: " + x);
+
+    x += 3;  // x = x + 3
+    console.log("Setelah x += 3 : " + x);
+
+    x -= 2;  // x = x - 2
+    console.log("Setelah x -= 2 : " + x);
+
+    x *= 4;  // x = x * 4
+    console.log("Setelah x *= 4 : " + x);
+
+    x /= 2;  // x = x / 2
+    console.log("Setelah x /= 2 : " + x);
+
+    x %= 3;  // x = x % 3
+    console.log("Setelah x %= 3 : " + x);
+}
+
+contohAssignment();
