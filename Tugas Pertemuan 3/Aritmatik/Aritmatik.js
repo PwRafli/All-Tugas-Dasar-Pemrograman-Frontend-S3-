ccda74b8ae0931c7cf791a1986b4a9c69c029ad6
PwRafli <rafliefendymm@gmail.com>
2025-09-30 10:01:05 +0700

    Add files via upload

---
+++ Tugas Pertemuan 3/Aritmatik/Aritmatik.js
@@ -0,0 +1,10 @@
+// Contoh Operator Aritmatika
+function hitungAritmatik(a, b) {
+    console.log("Penjumlahan: " + (a + b));
+    console.log("Pengurangan: " + (a - b));
+    console.log("Perkalian: " + (a * b));
+    console.log("Pembagian: " + (a / b));
+    console.log("Modulus: " + (a % b));
+}
+
+hitungAritmatik(10, 3);
